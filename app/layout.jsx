e8ddef9f50ec0014ac7d216ee4143551302c124d
i <i@example.com>
2024-07-21 19:03:16 +0300

    f

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,86 @@
 import "./globals.css";
+import Head from 'next/head';
 import Header from "@/components/header/page";
 import Footer from "@/components/footer/page";
 
 export const metadata = {
-  title: "Юнікейс - Адвокатське Об'єднання - Головна",
-  description: "Юнікейс - Адвокатське Об'єднання",
+  title: "Юнікейс - Адвокатське Об'єднання",
+  description: "Отримайте юридичну допомогу",
+  openGraph: {
+    title: "АО Юнікейс",
+    description: "Отримайте правову допомогу",
+    image: "https://www.aounicase.com.ua/unicase.webp",
+    url: "https://www.aounicase.com.ua",
+    type: "website",
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="uk">
+      <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16593300850"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16593300850');
+          `
+        }} />
+
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KD5RQWHG');
+          `
+        }} />
+      </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KD5RQWHG" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
         <div className="container">
           {children}
         </div>
         <Footer />
       </body>
-    </html >
+    </html>
   );
 }
+/*
+/**|======================================
+/**| old code
+/**|======================================
+*/
+// import "./globals.css";
+// import Header from "@/components/header/page";
+// import Footer from "@/components/footer/page";
+
+// export const metadata = {
+//   title: "Юнікейс - Адвокатське Об'єднання - Головна",
+//   description: "Отримайте юридичну допомогу",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="uk">
+//       <body>
+//         <Header />
+//         <div className="container">
+//           {children}
+//         </div>
+//         <Footer />
+//       </body>
+//     </html >
+//   );
+// }
